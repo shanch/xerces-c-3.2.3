8c56c35841b1153364d331bfd9a95a186e28e2b0
shanch <shanch@sweet.ocn.ne.jp>
2020-04-22 15:36:58 +0900

    プロジェクト ファイルを追加します。

---
+++ CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: E:/Temp/xerces-c-3.2.3
+# Build directory: E:/Temp/xerces-c-3.2.3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("doc")
+subdirs("src")
+subdirs("tests")
+subdirs("samples")
